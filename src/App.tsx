50668d86b66ef8fb5ccf337118a8dae5d69804b4
RenannLimaa <renanroddev@gmail.com>
2025-11-10 12:13:38 -0300

    feat: add missions route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Index from './pages/Index.tsx'
+import Missions from './pages/Missions.tsx'
 
 function App() {
   return (
     <Routes>
-      <Route path="/index" element={<Index />} />
+      <Route path="/" element={<Index />} />
+      <Route path="/missions" element={<Missions />} />
     </Routes>
   )
 }
